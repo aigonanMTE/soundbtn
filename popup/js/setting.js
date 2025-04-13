dd7575b194b0295b84e880b9ab8a900f16198b75
MTE <93072523+aigonanMTE@users.noreply.github.com>
2025-04-13 13:11:13 +0900

    텝 어케염?
    ㅇㅇ

--- popup/js/setting.js
+++ popup/js/setting.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     const settingbtn = document.getElementById('settingbtn');
-    
+
+    settingbtn.addEventListener('click', function () {
+        chrome.tabs.create({
+            url: chrome.runtime.getURL("C:/Users/a0109/Desktop/setting.html") // 경로에 맞게 수정
+        });
+    });
 });
